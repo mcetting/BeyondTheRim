a9ef4e7f81db1234c82cc1f6ec5eb5535011c69d
Michael Ettinger <mcetting@ucsc.edu>
2019-01-14 20:32:51 -0800

    added game and object manager

---
+++ BTR/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton of GameManger script for out of class acess
+    private static GameManager instance;
+    public static GameManager Instance { get; private set; }
+
+    // for script value initializing because it runs before start
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // first iteration of spawning an object in the gameworld
+    public void SpawnObjectAtPosition(GameObject obj, Vector3 position)
+    {
+        GameObject newObj = Instantiate(obj);
+        newObj.transform.position = position;
+    }
+}
