1874ae8eacedab3c30a9bce173cd84066f0877f9
Michael Ettinger <mcetting@ucsc.edu>
2019-01-14 22:19:42 -0800

    added a player that spawns with a camera and can move

---
+++ BTR/Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// only handles player movement and interactions with input
+// camera controller will be another script on this object
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private CharacterController cc;
+    private Vector2 inputVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Movement();
+    }
+
+    // store the input in a vector for later use
+    private void GetInput()
+    {
+        inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    // move the character according to the input
+    private void Movement()
+    {
+        Vector3 movement = new Vector3();
+        movement += transform.forward * inputVector.y * moveSpeed * Time.deltaTime;
+        movement += transform.right   * inputVector.x * moveSpeed * Time.deltaTime;
+        cc.Move(movement);
+    }
+}
